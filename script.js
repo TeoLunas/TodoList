18afad1640c48d0f13965abc546366b670a84ce9
Teo <teo@MacBook-Pro-de-Teo.local>
2021-09-30 00:52:53 -0300

    Se mejora estilos de css

--- script.js
+++ script.js
@@ -20,12 +20,36 @@ function crearTarea() {
 
 //Crear HTML para mostrar tarea.
 
-function crearTareaHtml(t){
+/*function crearTareaHtml(t){
     let nuevoParrafo = document.createElement('p');
     let contenido = document.createTextNode(t)
     nuevoParrafo.appendChild(contenido);
+    nuevoParrafo.classList.add('border', 'border-secondary', 'rounded-pill', 'w-50', 'text-center', 'mt-3')
     resultado.appendChild(nuevoParrafo);
-};
+};*/
+
+function crearTareaHtml(t){
+    const div = document.createElement('div');
+    const parrafo = document.createElement('p');
+    const iconoUno = document.createElement('i');
+    const iconoDos = document.createElement('i');
+
+    const contenidoParrafo = document.createTextNode(t);
+
+    div.classList.add('task');
+    iconoUno.classList.add('far', 'fa-check-square');
+    iconoDos.classList.add('fas', 'fa-trash');
+
+
+    parrafo.appendChild(contenidoParrafo);
+
+    div.appendChild(parrafo);
+    div.appendChild(iconoUno);
+    div.appendChild(iconoDos);
+    
+    resultado.appendChild(div)
+
+}
 
 function limpiarHtml(){
     while(resultado.firstChild){
