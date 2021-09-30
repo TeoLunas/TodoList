1fd19af37ecf2315ef44cf1eb78f0454635971bf
Teo <teo@MacBook-Pro-de-Teo.local>
2021-09-30 01:49:30 -0300

    se ocambian nombres de funciones para redistribuir que hacia cada una, se guardan datos en localstorage para obtener per en los datos. Se muestran estos datos

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
-const tarea = document.getElementById('tarea');
+const tareaInpu = document.getElementById('tarea');
 const form = document.getElementById('form-data');
 const resultado = document.getElementById('tareasPorHacer');
 
-const tareas = [];
+let tareas = [];
 
-function crearTarea() {
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        limpiarHtml();
-        let task = tarea.value;
-        tareas.push({'id': Date.now(), 'tarea' :task, estado : false})
-        tareas.forEach(e => {
-            crearTareaHtml(e.tarea)
-        });
-        
-        //console.log(tareas);
-    });
+function documentoCargado() {
+    form.addEventListener('submit', AgregarTarea);
+
+    document.addEventListener('DOMContentLoaded', function(){
+
+        tareas = JSON.parse(localStorage.getItem('task')) || [];
+
+        crearTareaHtml();
+
+    })
+}
+
+function AgregarTarea(e) {
+    e.preventDefault()
+    limpiarHtml();
+    let task = tareaInpu.value;
+    tareas.push({ 'id': Date.now(), 'tarea': task, estado: false });
+    crearTareaHtml();
+
 }
 
 //Crear HTML para mostrar tarea.
 
-/*function crearTareaHtml(t){
-    let nuevoParrafo = document.createElement('p');
-    let contenido = document.createTextNode(t)
-    nuevoParrafo.appendChild(contenido);
-    nuevoParrafo.classList.add('border', 'border-secondary', 'rounded-pill', 'w-50', 'text-center', 'mt-3')
-    resultado.appendChild(nuevoParrafo);
-};*/
+function crearTareaHtml() {
 
-function crearTareaHtml(t){
-    const div = document.createElement('div');
-    const parrafo = document.createElement('p');
-    const iconoUno = document.createElement('i');
-    const iconoDos = document.createElement('i');
+    if(tareas.length > 0){
+        
+        tareas.forEach(e => {
+            const div = document.createElement('div');
+            const parrafo = document.createElement('p');
+            const iconoUno = document.createElement('i');
+            const iconoDos = document.createElement('i');
+        
+            const contenidoParrafo = document.createTextNode(e.tarea);
+        
+            div.classList.add('task');
+            iconoUno.classList.add('far', 'fa-check-square');
+            iconoDos.classList.add('fas', 'fa-trash');
+        
+        
+            parrafo.appendChild(contenidoParrafo);
+        
+            div.appendChild(parrafo);
+            div.appendChild(iconoUno);
+            div.appendChild(iconoDos);
+        
+            resultado.appendChild(div)
+        })
+    }
 
-    const contenidoParrafo = document.createTextNode(t);
-
-    div.classList.add('task');
-    iconoUno.classList.add('far', 'fa-check-square');
-    iconoDos.classList.add('fas', 'fa-trash');
-
-
-    parrafo.appendChild(contenidoParrafo);
-
-    div.appendChild(parrafo);
-    div.appendChild(iconoUno);
-    div.appendChild(iconoDos);
-    
-    resultado.appendChild(div)
-
+    enviarDatosLS();
 }
 
-function limpiarHtml(){
-    while(resultado.firstChild){
+function enviarDatosLS(){
+    localStorage.setItem('task', JSON.stringify(tareas));
+}
+
+function limpiarHtml() {
+    while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
 }
@@ -60,4 +70,4 @@ function limpiarHtml(){
 //Registrar tarea en arreglo
 
 
-crearTarea();
+documentoCargado();
