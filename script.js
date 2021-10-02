a03b0e2dd11f8193d59391c13c76f00fa8a03f26
Teo <teo@MacBook-Pro-de-Teo.local>
2021-10-02 14:32:24 -0300

    se crea sesccion trea completas, ademas se agregan tareas a esta seccion cuando estan completadas

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const tareaInpu = document.getElementById('tarea');
 const form = document.getElementById('form-data');
 const resultado = document.getElementById('tareasPorHacer');
 const inputTask = document.getElementById('inputTask');
+const totalTareas = document.getElementById('totalTareas');
+const tareasCompletadas = document.getElementById('tareasCompletadas');
 
 let tareas = [];
+let tareasTerminadas = [];
 
 function documentoCargado() {
     form.addEventListener('submit', AgregarTarea);
 
-    document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('DOMContentLoaded', function () {
 
         tareas = JSON.parse(localStorage.getItem('task')) || [];
-
+        
         crearTareaHtml();
 
     })
@@ -21,11 +24,11 @@ function AgregarTarea(e) {
     e.preventDefault()
     limpiarHtml();
     let task = tareaInpu.value;
-    if(task === ""){
+    if (task === "") {
         error();
         crearTareaHtml();
         form.reset();
-    }else{
+    } else {
         tareas.push({ 'id': Date.now(), 'tarea': task, estado: false });
         crearTareaHtml();
         form.reset();
@@ -36,35 +39,53 @@ function AgregarTarea(e) {
 
 function crearTareaHtml() {
 
-    if(tareas.length > 0){
-        
+    if (tareas.length > 0) {
+
         tareas.forEach(e => {
             const div = document.createElement('div');
             const parrafo = document.createElement('p');
-            const iconoUno = document.createElement('i');
-            const iconoDos = document.createElement('i');
-        
+            //const iconoUno = document.createElement('i');
+            //const iconoDos = document.createElement('i');
+            const iconoCompletar = document.createElement('a');
+            const iconoEliminar = document.createElement('a');
+
+
             const contenidoParrafo = document.createTextNode(e.tarea);
-        
+
             div.classList.add('task');
-            iconoUno.classList.add('far', 'fa-check-square');
-            iconoDos.classList.add('fas', 'fa-trash');
-        
-        
+            //iconoUno.classList.add('far', 'fa-check-square');
+            //iconoDos.classList.add('fas', 'fa-trash');
+            iconoCompletar.classList.add('far', 'fa-check-square');
+            iconoEliminar.classList.add('fas', 'fa-trash');
+
+            iconoCompletar.setAttribute('id', e.id)
+            iconoEliminar.setAttribute('id', e.id)
+
+            iconoCompletar.onclick = () => {
+                completarTarea(e.id);
+            }
+
             parrafo.appendChild(contenidoParrafo);
-        
+
             div.appendChild(parrafo);
-            div.appendChild(iconoUno);
-            div.appendChild(iconoDos);
-        
+            //div.appendChild(iconoUno);
+            //div.appendChild(iconoDos);
+            div.appendChild(iconoCompletar);
+            div.appendChild(iconoEliminar);
+
             resultado.appendChild(div)
+            if (e.estado === true) {
+                div.classList.add('tareaCompleta');
+                tareasCompletadas.appendChild(div);
+               
+            }
         })
     }
 
     enviarDatosLS();
 }
 
-function enviarDatosLS(){
+function enviarDatosLS() {
     localStorage.setItem('task', JSON.stringify(tareas));
 }
 
@@ -74,12 +95,39 @@ function limpiarHtml() {
     }
 }
 
-function error(){
+function clean(){
+    while(tareasCompletadas.firstChild){
+        tareasCompletadas.removeChild(tareasCompletadas.firstChild);
+    }
+}
+
+
+function error() {
     tareaInpu.classList.add('error')
-    setTimeout(function(){
+    setTimeout(function () {
         tareaInpu.classList.remove('error')
 
     }, 4000)
+}
+
+function completarTarea(id) {
+    tareas.forEach(e => {
+        if (id === e.id) {
+            e.estado = true;
+            clean();
+            limpiarHtml();
+            crearTareaHtml();
+            enviarDatosLS();
+        }
+    })
+
+}
+
+function cantidad(){
+    let t = tareas.length;
+    console.log(t);
+
+    totalTareas.value(t)
 }
 
 documentoCargado();
